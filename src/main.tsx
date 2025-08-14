b84cb0f2b9134927b144ae070122b50e0c0f540f
Jontybrown <jontydavidbrown@gmail.com>
2025-08-14 21:43:08 +1000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-
+import "./index.css";
 import App from "./App"
 import User from "./pages/User"
 
