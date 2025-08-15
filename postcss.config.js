7c7ee2692267def36a9a99764924e6d1bc671860
Jontybrown <jontydavidbrown@gmail.com>
2025-08-15 10:54:16 +0000

    chore: add Tailwind/PostCSS config and deps

--- postcss.config.js
+++ postcss.config.js
@@ -1,2 +1,9 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+};
+
 
 export default { plugins: { tailwindcss: {}, autoprefixer: {} } }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: { extend: {} },
+  plugins: []
+};
 
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
