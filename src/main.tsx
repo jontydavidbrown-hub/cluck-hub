d2845315f82bbadbd6fdbfccfe42c899fbad100a
Jontybrown <jontydavidbrown@gmail.com>
2025-08-20 18:32:34 +1000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import Pickups from "./pages/Pickups";
 
 import Dashboard from "./pages/Dashboard";
 import Morts from "./pages/Morts";
